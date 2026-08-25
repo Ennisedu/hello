d0c1f0c0e7888b8a830c8c8d39ef1b01e0b1f26b
Nia Ennis <nennis2@huskers.unl.edu>
2026-08-25 13:28:58 -0500

    intial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nia Ennis
+ * Date: 2026/08/25
+ *
+ * A simple my name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nia Ennis\n");
+
+  return 0;
+}
